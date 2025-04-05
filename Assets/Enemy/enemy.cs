b2f0cba1a60576e5a47812c23d31686530906a5f
hoichael <emurenn@gmail.com>
2025-04-05 09:23:15 +0200

    enemy rework

--- Assets/Enemy/enemy.cs
+++ Assets/Enemy/enemy.cs
@@ -1,13 +1,16 @@
 using UnityEngine;
 
-public class en_rotate : MonoBehaviour
+public class enemy : MonoBehaviour
 {
     [SerializeField] Rigidbody rb;
+    [SerializeField] float move_speed;
 
     void FixedUpdate()
     {
         Vector3 dir_to_pl = (g_refs.i.pl_trans.position + Vector3.up) - transform.position;
 
         rb.rotation = Quaternion.LookRotation(dir_to_pl, Vector3.up);
+
+        rb.AddForce(dir_to_pl.normalized * move_speed);
     }
 }
