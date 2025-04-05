107e39540f68aa309ad53bb297aec448f3633193
hoichael <emurenn@gmail.com>
2025-04-05 14:13:24 +0200

    ui_circle

---
+++ Assets/Rendering/ui_circle.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class ui_circle : MonoBehaviour
+{
+    [Header("REFS")]
+    [SerializeField] LineRenderer line;
+
+    [Header("SETTINGS")]
+    [SerializeField] int subdivisions;
+    [SerializeField] float radius;
+
+    void Update()
+    {
+        update_circle();
+    }
+
+    void update_circle()
+    {
+        line.positionCount = subdivisions;
+
+        float angle_step = 2f * Mathf.PI / subdivisions;
+
+        for(int i = 0; i < subdivisions; i++)
+        {
+            float pos_x = radius * Mathf.Cos(angle_step * i);
+            float pos_z = radius * Mathf.Sin(angle_step * i);
+
+            Vector3 point = new Vector3(pos_x, 0f, pos_z);
+            line.SetPosition(i, point);
+        }
+    }
+}
