ed39319c34ab432502f9b070fe2da67c74098bb1
hoichael <emurenn@gmail.com>
2025-04-05 08:25:59 +0200

    room_manager basic flow

--- Assets/Room/room.cs
+++ Assets/Room/room.cs
@@ -6,11 +6,17 @@ public class room : MonoBehaviour
     [SerializeField] GameObject container;
 
     [Header("SETTINGS")]
-    [SerializeField] Transform trans_pl_spawn_ref;
+    [field:SerializeField] public Transform trans_pl_spawn_ref { get; private set; }
+
+    public void init()
+    {
+        container.SetActive(true);
+    }
 
     public void on_exit_trigger()
     {
         print("ROOM EXIT! (from room)");
+        container.SetActive(false);
         g_refs.i.room_manager.on_room_exit();
     }
 }
--- Assets/Room/room_manager.cs
+++ Assets/Room/room_manager.cs
@@ -7,21 +7,27 @@ public class room_manager : MonoBehaviour
     [SerializeField] List<room_layer_list> list_layer = new List<room_layer_list>();
 
     int layer_current;
-    int layer_max;
+    int layer_max = 1337;
 
     public void on_room_exit()
     {
         print("ROOM EXIT! (from manager)");
+        layer_current = Mathf.Clamp(layer_current + 1, 0, layer_max);
+        enter_new_room();
     }
 
     void enter_new_room()
     {
+        int idx_random = 0;
 
+        room room_new = list_layer[layer_current].list_room[idx_random];
+        room_new.init();     
+        g_refs.i.pl_trans.GetComponent<Rigidbody>().position = room_new.trans_pl_spawn_ref.position;
     }
 }
 
 [System.Serializable]
 public class room_layer_list
 {
-    [SerializeField] List<room> list_room;
+    [field:SerializeField] public List<room> list_room { get; private set; }
 }
