d4c95f5253af6c9ae39ab7ded244042bb4af1dd2
hoichael <emurenn@gmail.com>
2025-04-05 06:53:15 +0200

    pl_move dynamic rb drag

--- Assets/Player/pl_move.cs
+++ Assets/Player/pl_move.cs
@@ -8,17 +8,23 @@ public class pl_move : MonoBehaviour
 
     [Header("SETTINGS")]
     [SerializeField] float move_force_ground;
+    [SerializeField] float move_force_air;
+
+    [SerializeField] float drag_ground, drag_air;
 
     void FixedUpdate()
     {
         if (refs.groundcheck.is_grounded())
         {
             refs.gravity.enabled = false;
+            refs.rb.linearDamping = drag_ground;
             apply_move_ground(get_dir());
         }
         else
         {
             refs.gravity.enabled = true;
+            refs.rb.linearDamping = drag_air;
+            apply_move_air(get_dir());
         }
     }
 
@@ -36,5 +42,10 @@ public class pl_move : MonoBehaviour
     void apply_move_ground(Vector3 dir)
     {
         refs.rb.AddForce(dir * move_force_ground);
+    }
+
+    void apply_move_air(Vector3 dir)
+    {
+        refs.rb.AddForce(dir * move_force_air);
     }
 }
