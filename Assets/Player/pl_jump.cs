dbbca08566ae0afa7fcc4d7ce8c1562412a90e51
hoichael <emurenn@gmail.com>
2025-04-06 11:53:55 +0200

    minor jump stuff

--- Assets/Player/pl_jump.cs
+++ Assets/Player/pl_jump.cs
@@ -1,21 +1,24 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Collections.Generic;
 
 public class pl_jump : MonoBehaviour
 {
     [Header("REFS")]
     [SerializeField] pl_refs refs;
-    [SerializeField] AudioSource audiosrc;
+    [SerializeField] List<AudioSource> list_audiosrc;
 
     [Header("SETTINGS")]
     [SerializeField] float force;
+
+    int sfx_last_idx;
 
     void Update()
     {
         if (InputSystem.actions.FindAction("Jump").WasPressedThisFrame() && refs.groundcheck.is_grounded())
         {
             exec_jump();
-            audiosrc.Play();
+            handle_sfx();
         }
     }
 
@@ -23,5 +26,17 @@ public class pl_jump : MonoBehaviour
     {
         refs.rb.linearVelocity = new Vector3(refs.rb.linearVelocity.x, 0, refs.rb.linearVelocity.z);
         refs.rb.AddForce(Vector3.up * force, ForceMode.Impulse);
+    }
+
+    void handle_sfx()
+    {
+        sfx_last_idx++;
+
+        if (sfx_last_idx == list_audiosrc.Count)
+        {
+            sfx_last_idx = 0;
+        }
+
+        list_audiosrc[sfx_last_idx].Play();
     }
 }
