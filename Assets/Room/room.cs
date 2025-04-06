8e53ee9eecd6add410b07699eeb10a1f37e40395
hoichael <emurenn@gmail.com>
2025-04-06 06:28:03 +0200

    Progress.

--- Assets/Room/room.cs
+++ Assets/Room/room.cs
@@ -1,12 +1,32 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class room : MonoBehaviour
 {
     [Header("REFS")]
     [SerializeField] GameObject container;
+    [SerializeField] List<Terrain> list_terrain;
 
     [Header("SETTINGS")]
     [field:SerializeField] public Transform trans_pl_spawn_ref { get; private set; }
+    [SerializeField] Material mat_terrain;
+
+    void Start()
+    {
+        for(int i = 0; i < list_terrain.Count; i++)
+        {
+
+            list_terrain[i].gameObject.SetActive(true);
+            list_terrain[i].gameObject.layer = LayerMask.NameToLayer("Solid");
+
+            list_terrain[i].materialTemplate = mat_terrain; 
+
+            if(i != 0)
+            {
+                list_terrain[i].GetComponent<TerrainCollider>().enabled = false;
+            }
+        }    
+    }
 
     public void init()
     {
